fe6ac17672a26afc9ba4fe3f761f871ebcc34c61
zaidkhan0997 <kzaid0997@gmail.com>
2023-01-11 20:31:48 +0530

    Revert "sm6150-common: switch to azure-clang 15"
    This reverts commit 9e741703b27a4101ceb519ad57c969b7aa9256ff.
    
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,14 +82,12 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
-TARGET_KERNEL_CLANG_VERSION := azure
-TARGET_KERNEL_CLANG_COMPILE := true
-KERNEL_SUPPORTS_LLVM_TOOLS := true
+TARGET_KERNEL_CLANG_VERSION := playground
+TARGET_CLANG_PREBUILTS_VERSION := playground
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-playground
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     LLVM=1 \
     LLVM_IAS=1
-
-KERNEL_LD := LD=ld.lld
 
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x880000
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom androidboot.console=ttyMSM0
