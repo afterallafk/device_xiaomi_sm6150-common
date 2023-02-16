7b68b2a11ca356e66f0e05ab7cd22f98f889fb86
AndroidHQ254 <d.murimi14@gmail.com>
2023-02-16 19:53:45 +0530

    sm6150-common: sepolicy: miuicamera: Address sepolicy denials
    E SELinux : avc:  denied  { find } for interface=vendor.xiaomi.hardware.misys::IMiSys sid=u:r:platform_app:s0:c512,c768 pid=7115 scontext=u:r:platform_app:s0:c512,c768 tcontext=u:object_r:default_android_hwservice:s0 tclass=hwservice_manager permissive=0
    
    E SELinux : avc:  denied  { find } for interface=vendor.xiaomi.hardware.campostproc::IMiPostProcService sid=u:r:platform_app:s0:c512,c768 pid=7115 scontext=u:r:platform_app:s0:c512,c768 tcontext=u:object_r:hal_camerapostproc_xiaomi_hwservice:s0 tclass=hwservice_manager permissive=0
    
    E SELinux : avc:  denied  { find } for pid=7115 uid=10126 name=miui_multi_display scontext=u:r:platform_app:s0:c512,c768 tcontext=u:object_r:default_android_service:s0 tclass=service_manager permissive=0
    
    E vndksupport: Could not load /vendor/lib64/libmialgoengine.so from sphal namespace: dlopen failed: library "/vendor/lib64/libmialgoengine.so" not found.
    
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,7 +148,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/qcom/sepolicy_vndr/SEPolicy.mk
-
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
